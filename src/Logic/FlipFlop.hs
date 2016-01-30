8e03b9a4c247714e503722acb0cc42de026d34f3
eijian <akagi.eiji.us@gmail.com>
2016-01-30 23:46:28 +0900

    fixed flip flop

--- src/Logic/FlipFlop.hs
+++ src/Logic/FlipFlop.hs
@@ -34,6 +34,8 @@ lc_srff (s:r:q:q':_) = [x, x']
     x_ = head $ lc_nand [s, q']
     x' = head $ lc_nand [r, x_]
     x  = head $ lc_nand [s, x']
+lc_srff (s:r:_) = lc_srff [s, r, sLO, sLO]
+lc_srff _ = lc_srff [sHI, sHI, sLO, sLO]
 
 {- |
 D-FlipFlop (edge trigger)
@@ -59,4 +61,38 @@ lc_dff' :: LogicCircuit
 lc_dff' (False:_)         = [sLO, sHI]
 lc_dff' (True :False:_)   = [sHI, sLO]
 lc_dff' (_    :_    :d:_) = [d, not d]
+
+{- |
+
+>>> lc_dff [sHI, sHI, sHI] == [sHI,sLO]
+True
+>>> lc_dff [sHI, sHI, sLO] == [sLO,sHI]
+True
+>>> lc_dff [sLO, sHI, sHI] == [sLO,sHI]
+True
+>>> lc_dff [sLO, sLO, sHI] == [sLO,sHI]
+True
+>>> lc_dff [sHI, sLO, sLO] == [sHI,sLO]
+True
+>>> lc_dff [sHI, sLO, sHI] == [sHI,sLO]
+True
+-}
+
+lc_dff :: LogicCircuit
+lc_dff [] = lc_dff [sLO]
+--lc_dff (False:_) = lc_srff [sLO, sHI, sLO]
+--lc_dff (True:False:_) = lc_srff [sHI, sLO, sLO]
+lc_dff (c:r:d:_) = lc_srff (x1' ++ x2' ++ q)
+  where
+    d' = lc_and ([c] ++ lc_or (lc_not [r] ++ [d]))
+    -- before clock
+    x1 = sHI  -- because C = LO
+    x2 = sHI  -- because C = LO
+    x3 = lc_nand (d' ++ [x2])
+    x0 = lc_nand ([x1] ++ x3)
+    q = lc_srff [x1, x2, sLO, sHI]  -- default Q = LO, Q'= HI
+    -- clock in (C = HI)
+    x1' = lc_nand ([sHI] ++ x0)
+    x2' = lc_nand ([sHI] ++ x1' ++ x3)
+
 
