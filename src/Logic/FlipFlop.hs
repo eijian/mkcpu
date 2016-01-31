66c4f996a66e1e703b748b9bfa0917e520ae24cf
eijian <akagi.eiji.us@gmail.com>
2016-02-01 07:35:30 +0900

    update D-FF

--- src/Logic/FlipFlop.hs
+++ src/Logic/FlipFlop.hs
@@ -85,12 +85,12 @@ lc_dff [] = lc_dff [sLO]
 lc_dff (c:r:d:_) = lc_srff (x1' ++ x2' ++ q)
   where
     d' = lc_and ([c] ++ lc_or (lc_not [r] ++ [d]))
-    -- before clock
-    x1 = sHI  -- because C = LO
-    x2 = sHI  -- because C = LO
-    x3 = lc_nand (d' ++ [x2])
-    x0 = lc_nand ([x1] ++ x3)
-    q = lc_srff [x1, x2, sLO, sHI]  -- default Q = LO, Q'= HI
+    -- before clock (C = LO)
+    x1 = [sHI]  -- because C = LO
+    x2 = [sHI]  -- because C = LO
+    x3 = lc_nand (d' ++ x2)
+    x0 = lc_nand (x1 ++ x3)
+    q = lc_srff (x1 ++ x2 ++ [sLO, sHI])  -- default Q = LO, Q'= HI
     -- clock in (C = HI)
     x1' = lc_nand ([sHI] ++ x0)
     x2' = lc_nand ([sHI] ++ x1' ++ x3)
