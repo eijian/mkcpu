4c0a534482655caf05e1cec41cb86c5165cc489e
eijian <akagi.eiji.us@gmail.com>
2016-04-14 20:43:19 +0900

    update main

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Control.Concurrent
+import System.Environment
 
 import Logic.BasicGate
 import Logic.FlipFlop
@@ -9,20 +10,30 @@ import Logic.ComplexLogic
 import Logic.Register
 
 wsec :: Int
-wsec = 1                  -- wait 1 sec
+wsec = 1                    -- wait 1 sec
 
-inistat :: [Bin]
-inistat = toBits "0000"   -- initial state of Program Counter
+defInput :: [Bin]
+defInput  = toBits "0000"   -- default value of Input port
 
 main :: IO ()
-main = loop lc_counter4 inistat
+main = do
+  inistat <- getContents
+  opts <- getArgs
+  let (wait, iport) = parseOpts opts
+  loop lc_counter4 (toBits inistat) wait
 
-loop :: LogicCircuit -> [Bin] -> IO ()
-loop lc is = do
+parseOpts :: [String] -> (Int, [Bin])
+parseOpts [] = (wsec, defInput)
+parseOpts (x:zs) = ((read :: String -> Int) x, defInput)
+
+loop :: LogicCircuit -> [Bin] -> Int -> IO ()
+loop lc is w = do
   let os = lc ([sHI, sHI] ++ is ++ toBits "0000")
   putStatus os
-  threadDelay (wsec * 1000 * 1000)
-  loop lc os
+  threadDelay (w * 1000 * 1000)
+  loop lc os w
+
+
 
 putStatus :: [Bin] -> IO ()
 putStatus os = putStrLn $ toStr os
